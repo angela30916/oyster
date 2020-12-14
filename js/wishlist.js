3df85940a9371207e6c4437fc8f2f72c16366f1a
angela30916 <angela30916@gmail.com>
2020-12-14 21:20:56 +0800

    💄 Add country flags and UI of rating

--- js/wishlist.js
+++ js/wishlist.js
@@ -22,42 +22,52 @@
 //     })
 // }
 
-// const vistedBtn = document.querySelector('#visted')
-// const wishlistBtn = document.querySelector('#wishlist')
-// vistedBtn.addEventListener('click', (e) => {
-//     firebase.auth().onAuthStateChanged((user) => {
-//         if (user) {
-//             const uid = user.uid
-//             if (e.currentTarget.getAttribute('data') === '0') {
-//                 e.currentTarget.setAttribute('data', '1')
-//                 e.target.style.fill = '#ddca03'
-//                 // ANCHOR
-//                 const q = event.currentTarget.previousElementSibling.value
-//                     .toLowerCase()
-//                     .replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
-//                 let countriesNode = d3.selectAll('path title')._groups[0]
-//                 let countriesList = Object.keys(countriesNode)
-//                     .map((key) => [countriesNode[key]])
-//                     .map((item) => item[0].textContent)
-//                 let index = countriesList.findIndex((country) => country === q)
-//                 let target = countriesNode[index].parentNode
-//             } else {
-//                 e.currentTarget.setAttribute('data', '0')
-//                 e.target.style.fill = '#fff'
-//             }
-//         } else {
-//             console.log('User is signed out!')
-//         }
-//     })
-// })
+const vistedBtn = document.querySelector('#visted')
+const wishlistBtn = document.querySelector('#wishlist')
+vistedBtn.addEventListener('click', (e) => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // const uid = user.uid
+            if (e.currentTarget.getAttribute('data') === '0') {
+                e.currentTarget.setAttribute('data', '1')
+                e.target.style.fill = '#ddca03'
+                // ANCHOR
+                // const q = event.currentTarget.previousElementSibling.value
+                //     .toLowerCase()
+                //     .replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
+                // let countriesNode = d3.selectAll('path title')._groups[0]
+                // let countriesList = Object.keys(countriesNode)
+                //     .map((key) => [countriesNode[key]])
+                //     .map((item) => item[0].textContent)
+                // let index = countriesList.findIndex((country) => country === q)
+                // let target = countriesNode[index].parentNode
+            } else {
+                e.currentTarget.setAttribute('data', '0')
+                e.target.style.fill = '#fff'
+            }
+        } else {
+            console.log('User is signed out!')
+        }
+    })
+})
 
-// // TODO
-// wishlistBtn.addEventListener('click', (e) => {
-//     if (e.currentTarget.getAttribute('data') === '0') {
-//         e.currentTarget.setAttribute('data', '1')
-//         e.target.style.fill = '#ddca03'
-//     } else {
-//         e.currentTarget.setAttribute('data', '0')
-//         e.target.style.fill = '#fff'
-//     }
-// })
+// TODO
+wishlistBtn.addEventListener('click', (e) => {
+    if (e.currentTarget.getAttribute('data') === '0') {
+        e.currentTarget.setAttribute('data', '1')
+        e.target.style.fill = '#ddca03'
+    } else {
+        e.currentTarget.setAttribute('data', '0')
+        e.target.style.fill = '#fff'
+    }
+})
+
+const exploreBtn = document.querySelector('#exploreBtn')
+const exploreArea = document.querySelector('.exploreArea')
+exploreBtn.addEventListener('click', () => {
+    if (exploreArea.style.display === 'none') {
+        exploreArea.style.display = 'block'
+    } else {
+        exploreArea.style.display = 'none'
+    }
+})
